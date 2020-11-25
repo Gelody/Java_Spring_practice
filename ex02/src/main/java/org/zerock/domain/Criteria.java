e000177a510e769debe6dfb05841b9f7f0d6ee59
gelody <bbirds94@gmail.com>
2020-11-25 21:51:34 +0900

    [Add] paging

--- ex02/src/main/java/org/zerock/domain/Criteria.java
+++ ex02/src/main/java/org/zerock/domain/Criteria.java
@@ -14,11 +14,12 @@ public class Criteria {
 	private int amount;
 	
 	public Criteria() {
-		this(1, 10);
+		this(1,10);
 	}
 	
 	public Criteria(int pageNum, int amount) {
 		this.pageNum = pageNum;
 		this.amount = amount;
 	}
+	
 }
