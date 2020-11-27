a85ac50e469eedc8d0f05e9c07682ef6b3d722f6
gelody <bbirds94@gmail.com>
2020-11-27 15:12:05 +0900

    [Add] page numbering

--- ex02/src/test/java/org/zerock/controller/BoardControllerTest.java
+++ ex02/src/test/java/org/zerock/controller/BoardControllerTest.java
@@ -46,15 +46,19 @@ public class BoardControllerTest {
 	}
 	
 	@Test
-	public void testList() throws Exception {
+	public void testListPaging() throws Exception {
 		
 		log.info(
-				mockMvc.perform(MockMvcRequestBuilders.get("/board/list"))
+				mockMvc.perform(MockMvcRequestBuilders.get("/board/list")
+				.param("pageNum", "2")
+				.param("amount", "50"))
 				.andReturn()
 				.getModelAndView()
 				.getModelMap());
 		
 	}
+	
+
 	
 //	@Test
 //	public void testRegister()throws Exception {
